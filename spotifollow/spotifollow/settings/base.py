323e30f635c2f6c94fcf17e97a0aa8a707945420
saminahussain <saminahu@gmail.com>
2021-06-24 18:24:53 -0700

    not sure update for heroku

--- spotifollow/spotifollow/settings/base.py
+++ spotifollow/spotifollow/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
 from decouple import config
 
@@ -171,3 +171,6 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
